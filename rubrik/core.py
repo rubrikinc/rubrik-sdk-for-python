7e464c4b99d3f214703c19b89c46bfdf6df7e57c
Drew Russell <Drew.Russell@rubrik.com>
2018-07-04 18:59:13 -0500

    add doc string to core.cluster version

--- rubrik/core.py
+++ rubrik/core.py
@@ -4,6 +4,11 @@ class core():
     """
 
     def cluster_version(self):
+        """Retrieves software version of the Rubrik cluster
+
+        Returns:
+            dict -- Software version running on the cluster
+        """
 
         api_request = self.get('v1', '/cluster/me/version')
         return api_request
