a139cca2a605b44398bf1d536834167b66f1d17c
Drew Russell <Drew.Russell@rubrik.com>
2018-07-04 22:04:06 -0500

    add assert var check for cluster_version

--- rubrik/core.py
+++ rubrik/core.py
@@ -10,5 +10,14 @@ class core():
             dict -- Software version running on the cluster
         """
 
-        api_request = self.get('v1', '/cluster/me/version')
+        cluster_version_api_version = 'v1'
+        cluster_version_api_endpoint = '/cluster/me/version'
+
+        api_request = self.get(cluster_version_api_version, cluster_version_api_endpoint)
+
+        ########### DO NOT MODIFY THESE VALUES - USED IN UNIT TESTS ONLY #########
+        assert cluster_version_api_version == 'v1'
+        assert cluster_version_api_endpoint == '/cluster/me/version'
+        ##########################################################################
+
         return api_request
