ed30b1b52136853edb3a63387bdb90639e3322f4
Drew Russell <Drew.Russell@rubrik.com>
2018-07-16 22:09:20 -0500

    add bootstrap unit test

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -59,3 +59,65 @@ def test_bootstrap_status(rubrik_connect):
 
     with pytest.raises(SystemExit, match="500 Server Error: Internal Server Error for url"):
         resp = rubrik.bootstrap_status()
+
+
+@responses.activate
+def test_bootstrap(rubrik_connect):
+
+    rubrik = rubrik_connect
+
+    # node_config must be populated
+    with pytest.raises(SystemExit, match='Error: You must provide a valid dictionary for "node_config".'):
+        resp = rubrik.bootstrap("test-cluster", "test-email", "admin-password", "10.255.100.1", "255.255.255.0",
+                                enable_encryption=True, node_config=None, dns_search_domains=None, dns_nameservers=None, ntp_servers=None)
+    # node_config must be a dict
+    with pytest.raises(SystemExit, match='Error: You must provide a valid dictionary for "node_config".'):
+        resp = rubrik.bootstrap("test-cluster", "test-email", "admin-password", "10.255.100.1", "255.255.255.0",
+                                enable_encryption=True, node_config="Incorrect_Config_Type", dns_search_domains=None, dns_nameservers=None, ntp_servers=None)
+    # dns_search_domains must be a dic
+    with pytest.raises(SystemExit, match='Error: You must provide a valid list for "dns_search_domains".'):
+        resp = rubrik.bootstrap("test-cluster", "test-email", "admin-password", "10.255.100.1", "255.255.255.0",
+                                enable_encryption=True, node_config={}, dns_search_domains="Invalid_Search_Domain_Type", dns_nameservers=None, ntp_servers=None)
+    # dns_nameservers must be a dic
+    with pytest.raises(SystemExit, match='Error: You must provide a valid list for "dns_nameservers".'):
+        resp = rubrik.bootstrap("test-cluster", "test-email", "admin-password", "10.255.100.1", "255.255.255.0",
+                                enable_encryption=True, node_config={}, dns_search_domains=None, dns_nameservers="Invalid_Nameserver", ntp_servers=None)
+
+    # ntp_servers must be a dic
+    with pytest.raises(SystemExit, match='Error: You must provide a valid list for "ntp_servers".'):
+        resp = rubrik.bootstrap("test-cluster", "test-email", "admin-password", "10.255.100.1", "255.255.255.0",
+                                enable_encryption=True, node_config={}, dns_search_domains=None, dns_nameservers=None, ntp_servers="Invalid_NTP_Type")
+
+    api_version = 'internal'
+    api_endpoint = '/cluster/me/bootstrap'
+
+    url = "https://{}/api/{}{}".format(rubrik.node_ip, api_version, api_endpoint)
+
+    response_body = {
+        "status": "string",
+        "id": 0
+    }
+
+    cluster_name = "PythonSDK"
+    admin_email = "test@rubrikpythonsdk.com"
+    admin_password = "RubrikGoForward"
+    management_gateway = '172.31.0.1'
+    management_subnet_mask = '255.255.240.0'
+
+    node_config = {
+        '1': '172.98.9.245',
+        '2': '172.98.15.64',
+        '3': '172.98.0.117',
+        '4': '172.98.10.112'
+    }
+
+    responses.add(responses.POST, url, json=response_body, status=200)
+    responses.add(responses.POST, url, status=500)
+
+    resp = bootstrap = rubrik.bootstrap(cluster_name, admin_email, admin_password, management_gateway,
+                                        management_subnet_mask, enable_encryption=False, node_config=node_config)
+    assert resp == response_body
+
+    with pytest.raises(SystemExit, match="500 Server Error: Internal Server Error for url"):
+        resp = bootstrap = rubrik.bootstrap(cluster_name, admin_email, admin_password, management_gateway,
+                                            management_subnet_mask, enable_encryption=False, node_config=node_config)
