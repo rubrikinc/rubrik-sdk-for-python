43f0a6493ef2c694dcdcf1ac621a5eea7834648b
Drew Russell <Drew.Russell@rubrik.com>
2018-08-05 14:50:50 -0500

    update doc theme

--- docs/conf.py
+++ docs/conf.py
@@ -73,10 +73,17 @@ pygments_style = 'sphinx'
 
 import guzzle_sphinx_theme
 
-extensions.append("guzzle_sphinx_theme")
-html_translator_class = 'guzzle_sphinx_theme.HTMLTranslator'
 html_theme_path = guzzle_sphinx_theme.html_theme_path()
 html_theme = 'guzzle_sphinx_theme'
+
+# Register the theme as an extension to generate a sitemap.xml
+extensions.append("guzzle_sphinx_theme")
+
+# Guzzle theme options (see theme.conf for more information)
+html_theme_options = {
+    # Set the name of the project to appear in the sidebar
+    "project_nav_name": "Rubrik SDK for Python",
+}
 # Guzzle theme options (see theme.conf for more information)
 
 # -- Options for HTML output -------------------------------------------------
