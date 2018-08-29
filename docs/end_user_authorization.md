16f9406dd84f3d590dc2219c2d2ccfdb7f371ffe
Drew Russell <Drew.Russell@rubrik.com>
2018-08-29 13:28:06 -0500

    end_user_authorization updates

--- docs/end_user_authorization.md
+++ docs/end_user_authorization.md
@@ -1,6 +1,6 @@
 # end_user_authorization
 
-[summary]
+Grant an End User authorization to the provided object.
 ```py
 def end_user_authorization(object_name, end_user, object_type='vmware', timeout=15)
 ```
@@ -8,10 +8,16 @@ def end_user_authorization(object_name, end_user, object_type='vmware', timeout=
 ## Arguments
 | Name        | Type | Description                                                                 | Choices |
 |-------------|------|-----------------------------------------------------------------------------|---------|
-| object_name  | [type]  | [description] |         |
-| end_user  | [type]  | [description] |         |
+| object_name  | str  | The name of the object you wish to grant authorization to. |         |
+| end_user  | str  | The name of the end user you wish to grant authorization to. |         |
 ## Keyword Arguments
 | Name        | Type | Description                                                                 | Choices | Default |
 |-------------|------|-----------------------------------------------------------------------------|---------|---------|
-| object_type  | str  | [description] (default: {'vmware'}) |         |    'vmware'     |
-| timeout  | int  | [description] (default: {15}) |         |    15     |
+| object_type  | str  | The Rubrik object type you wish to backup. `vmware` is currently the only supported option. (choices: {vmware}) (default: {vmware}) |    vmware (default: {vmware     |    vmware     |
+| timeout  | int  | The timeout value for the API call that grants the End User authoriauthorizationation. (default: {15}) |         |    15     |
+
+## Returns
+| Type | Description                                                                                   |
+|------|-----------------------------------------------------------------------------------------------|
+| str  | If the End User is already authorized to interact with the provided object name the following is returned: The End User "{`end_user`}" is already authorized to interact with the "{`object_name`}" VM. |
+| dict  | The full response for the `/internal//authorization/role/end_user` API endpoint. |
--- rubrik/cluster.py
+++ rubrik/cluster.py
@@ -59,6 +59,20 @@ class Cluster(_API):
         return node_ip_list
 
     def end_user_authorization(self, object_name, end_user, object_type='vmware', timeout=15):
+        """Grant an End User authorization to the provided object.
+
+        Arguments:
+            object_name {str} -- The name of the object you wish to grant authorization to.
+            end_user {str} -- The name of the end user you wish to grant authorization to.
+
+        Keyword Arguments:
+            object_type {str} -- The Rubrik object type you wish to backup. `vmware` is currently the only supported option. (choices: {vmware}) (default: {vmware}) 
+            timeout {int} -- The timeout value for the API call that grants the End User authoriauthorizationation. (default: {15})
+
+        Returns:
+            str -- If the End User is already authorized to interact with the provided object name the following is returned: The End User "{`end_user`}" is already authorized to interact with the "{`object_name`}" VM.
+            dict -- The full response for the `/internal//authorization/role/end_user` API endpoint. 
+        """
 
         valid_object_type = ['vmware']
 
@@ -70,5 +84,22 @@ class Cluster(_API):
         vm_id = self.object_id(object_name, object_type)
 
         self.log("end_user_authorization: Searching the Rubrik Cluster for the End User '{}'.".format(end_user))
-        user_id = self.get('internal', '/user?username={}'.format(end_user))
-        print(user_id)
+        user = self.get('internal', '/user?username={}'.format(end_user))
+        if not user:
+            sys.exit('The Rubrik Cluster does not contain a End User Account named "{}".'.format(end_user))
+        else:
+            user_id = user[0]['id']
+
+        self.log("end_user_authorization: Searching the Rubrik Cluster for the End User '{}' authorizations.".format(end_user))
+        user_authorization = self.get('internal', '/authorization/role/end_user?principals={}'.format(user_id))
+
+        authorized_objects = user_authorization['data'][0]['privileges']['restore']
+
+        if vm_id in authorized_objects:
+            return 'The End User "{}" is already authorized to interact with the "{}" VM.'.format(end_user, object_name)
+        else:
+            config = {}
+            config['principals'] = [user_id]
+            config['privileges'] = {"restore": [vm_id]}
+
+            return self.post('internal', '/authorization/role/end_user', config, timeout=timeout)
