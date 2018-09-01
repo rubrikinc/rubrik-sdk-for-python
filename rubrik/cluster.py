a7df22fb0ff8ef855f21cc7051116d3890df08fa
Drew Russell <Drew.Russell@rubrik.com>
2018-09-01 15:35:40 -0500

    minor code updates to cluster.py

--- rubrik/cluster.py
+++ rubrik/cluster.py
@@ -22,30 +22,30 @@ _API = Api
 
 
 class Cluster(_API):
-    """This class contains methods related to the managment of the Rubrik Cluster itself.
+    """This class contains methods related to the managment of the Rubrik cluster itself.
     """
 
     def cluster_version(self, timeout=15):
-        """Retrieves the software version of the Rubrik Cluster.
+        """Retrieves the software version of the Rubrik cluster.
 
         Keyword Arguments:
-            timeout {int} -- The response timeout value, in seconds, of the API call. (default: {15})
+            timeout {int} -- The number of seconds to wait to establish a connection the Rubrik cluster before returning a timeout error. (default: {15})
 
         Returns:
-            dict -- Software version running on the Rubrik Cluster
+            dict -- The API response from `GET /v1/cluster/me/version`.
         """
-        self.log('cluster_version: Getting the software version of the Rubrik Cluster.')
-        api_request = self.get('v1', '/cluster/me/version', timeout)
-        return api_request
+
+        self.log('cluster_version: Getting the software version of the Rubrik cluster.')
+        return self.get('v1', '/cluster/me/version', timeout)
 
     def cluster_node_ip(self, timeout=15):
-        """Retrive the IP Address for each node in the Rubrik Cluster.
+        """Retrive the IP Address for each node in the Rubrik cluster.
 
         Keyword Arguments:
-            timeout {int} -- The response timeout value, in seconds, of the API call. (default: {15})
+            timeout {int} -- The number of seconds to wait to establish a connection the Rubrik cluster before returning a timeout error. (default: {15})
 
         Returns:
-            list -- A list that contains the IP Address for each node in the Rubrik Cluster.
+            list -- A list that contains the IP Address for each node in the Rubrik cluster.
         """
 
         self.log('cluster_node_ip: Generating a list of all Cluster Node IPs.')
@@ -62,16 +62,16 @@ class Cluster(_API):
         """Grant an End User authorization to the provided object.
 
         Arguments:
-            object_name {str} -- The name of the object you wish to grant authorization to.
+            object_name {str} -- The name of the object you wish to grant the `end_ser` authorization to.
             end_user {str} -- The name of the end user you wish to grant authorization to.
 
         Keyword Arguments:
-            object_type {str} -- The Rubrik object type you wish to backup. `vmware` is currently the only supported option. (default: {vmware}) (choices: {vmware}) 
-            timeout {int} -- The timeout value for the API call that grants the End User authoriauthorizationation. (default: {15})
+            object_type {str} -- The Rubrik object type you wish to backup. (default: {vmware}) (choices: {vmware}) 
+            timeout {int} -- The number of seconds to wait to establish a connection the Rubrik cluster before returning a timeout error. (default: {15})
 
         Returns:
-            str -- If the End User is already authorized to interact with the provided object name the following is returned: The End User "{`end_user`}" is already authorized to interact with the "`object_name`" VM.
-            dict -- The full response for the `/internal//authorization/role/end_user` API endpoint.
+            str -- 'No change required. The End User "`end_user`" is already authorized to interact with the "`object_name`" VM.'.format(end_user, object_name)
+            dict -- dict -- The API response from `POST /internal/authorization/role/end_user`.
         """
 
         valid_object_type = ['vmware']
@@ -80,23 +80,24 @@ class Cluster(_API):
             sys.exit("Error: The end_user_authorization() object_type argument must be one of the following: {}.".format(
                 valid_object_type))
 
-        self.log("end_user_authorization: Searching the Rubrik Cluster for the vSphere VM '{}'.".format(object_name))
+        self.log("end_user_authorization: Searching the Rubrik cluster for the vSphere VM '{}'.".format(object_name))
         vm_id = self.object_id(object_name, object_type)
 
-        self.log("end_user_authorization: Searching the Rubrik Cluster for the End User '{}'.".format(end_user))
+        self.log("end_user_authorization: Searching the Rubrik cluster for the End User '{}'.".format(end_user))
         user = self.get('internal', '/user?username={}'.format(end_user))
+
         if not user:
-            sys.exit('The Rubrik Cluster does not contain a End User Account named "{}".'.format(end_user))
+            sys.exit('The Rubrik cluster does not contain a End User account named "{}".'.format(end_user))
         else:
             user_id = user[0]['id']
 
-        self.log("end_user_authorization: Searching the Rubrik Cluster for the End User '{}' authorizations.".format(end_user))
+        self.log("end_user_authorization: Searching the Rubrik cluster for the End User '{}' authorizations.".format(end_user))
         user_authorization = self.get('internal', '/authorization/role/end_user?principals={}'.format(user_id))
 
         authorized_objects = user_authorization['data'][0]['privileges']['restore']
 
         if vm_id in authorized_objects:
-            return 'The End User "{}" is already authorized to interact with the "{}" VM.'.format(end_user, object_name)
+            return 'No change required. The End User "{}" is already authorized to interact with the "{}" VM.'.format(end_user, object_name)
         else:
             config = {}
             config['principals'] = [user_id]
