a3370e317abe26cce8b0cf425e9fe607e678ead8
Drew Russell <Drew.Russell@rubrik.com>
2019-01-12 15:36:10 -0600

    update user agent to include Rubrik

--- rubrik_cdm/rubrik_cdm.py
+++ rubrik_cdm/rubrik_cdm.py
@@ -121,7 +121,7 @@ class Connect(_CLUSTER, _DATA_MANAGEMENT, _PHYSICAL, _CLOUD):
             'Content-Type': 'application/json',
             'Accept': 'application/json',
             'Authorization': 'Basic ' + authorization,
-            'User-Agent': 'Python SDK v1.0.11'
+            'User-Agent': 'Rubrik Python SDK v1.0.11'
         }
 
         return authorization_header
