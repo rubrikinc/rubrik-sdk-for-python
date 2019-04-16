0602608a961009fbd950164cc7c404ec6767d3a8
Drew Russell <Drew.Russell@rubrik.com>
2019-04-15 22:42:53 -0500

    add unit test for cluster_version

--- tests/unit/cluster_test.py
+++ tests/unit/cluster_test.py
@@ -1,15 +1,14 @@
 import pytest
-from rubrik_cdm.exceptions import CDMVersionException
 
 
 @pytest.mark.unit
 def test_cluster_version_check(rubrik, monkeypatch):
 
-    def patch_cluster_version(timeout):
-        return "4.2.1-1417"
+    # Result of self.get('v1', '/cluster/me/version', timeout=timeout)
+    def patch_get_v1_cluster_me_version(api_version, api_endpoint, timeout):
+        return {'version': '5.0.1-1280'}
 
     # Monkey match rubrik.cluster_version to equal the patch_cluster_version() result
-    monkeypatch.setattr(rubrik, "cluster_version", patch_cluster_version)
+    monkeypatch.setattr(rubrik, "get", patch_get_v1_cluster_me_version)
 
-    with pytest.raises(CDMVersionException):
-        rubrik.cluster_version_check("5.0")
+    assert rubrik.cluster_version() == "5.0.1-1280"
