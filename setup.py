9b90c062070dd5a3fc05312d73309dfb33a16b94
Drew Russell <Drew.Russell@rubrik.com>
2019-05-31 23:02:55 -0500

    pypi updates

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="rubrik_cdm",
     version="2.0.0",
-    author="Rubrik Bio;d",
+    author="Rubrik Build",
     description="A Python package for interacting with the Rubrik CDM API.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.6"
     ],
     install_requires=[
         'requests >= 2.18.4',
