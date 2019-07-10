7805da85d5844f95f1077674fcf7c4690d3ce715
Drew Russell <Drew.Russell@rubrik.com>
2019-07-10 15:27:39 -0500

    add cdm_token to startup script

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -3,6 +3,8 @@
 
 # This script will automatically run after the container has been created.
 
+# Set environment variables template
 # echo "export rubrik_cdm_node_ip="" >> ~/.zshrc
 # echo "export rubrik_cdm_username="" >> ~/.zshrc
 # echo "export rubrik_cdm_password="" >> ~/.zshrc
+# echo "export rubrik_cdm_token="" >> ~/.zshrc
