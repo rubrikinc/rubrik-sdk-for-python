ebeaea6a034999e59910c4cc4267d87305040bc9
Drew Russell <Drew.Russell@rubrik.com>
2019-07-10 15:27:23 -0500

    vscode-add markdown extensions and startup script

---
+++ .devcontainer/start.sh
@@ -0,0 +1,8 @@
+
+#!/usr/bin/env bash
+
+# This script will automatically run after the container has been created.
+
+# echo "export rubrik_cdm_node_ip="" >> ~/.zshrc
+# echo "export rubrik_cdm_username="" >> ~/.zshrc
+# echo "export rubrik_cdm_password="" >> ~/.zshrc
