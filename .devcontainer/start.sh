7480fbf227ba719fb6d35f8055f8dedb309ce27a
Drew Russell <drew.russell@rubrik.com>
2019-07-13 09:57:03 -0500

    Update start.sh

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # This script will automatically run after the container has been created.
+# To untrack this file in git, run git update-index --skip-worktree .devcontainer/start.sh
 
 # Set environment variables template
 # echo "export rubrik_cdm_node_ip="" >> ~/.zshrc
