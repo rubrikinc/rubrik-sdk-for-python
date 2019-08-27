854e1086ef43c9fba0af01891043fc554db258db
Drew Russell <Drew.Russell@rubrik.com>
2019-08-27 13:17:02 +0000

    bump release version

--- rubrik_cdm/rubrik_cdm.py
+++ rubrik_cdm/rubrik_cdm.py
@@ -197,7 +197,7 @@ class Connect(Cluster, Data_Management, Physical, Cloud):
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
                 'Authorization': 'Basic ' + authorization,
-                'User-Agent': 'Rubrik Python SDK v2.0.2'
+                'User-Agent': 'Rubrik Python SDK v2.0.3'
             }
 
         else:
@@ -206,7 +206,7 @@ class Connect(Cluster, Data_Management, Physical, Cloud):
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
                 'Authorization': 'Bearer ' + self.api_token,
-                'User-Agent': 'Rubrik Python SDK v2.0.2'
+                'User-Agent': 'Rubrik Python SDK v2.0.3'
             }
 
         return authorization_header
