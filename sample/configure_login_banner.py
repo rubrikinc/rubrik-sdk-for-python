8f8ddb84c07cdae1f439d6a92cc6256bfaa99cd5
KowMangler <noman.wonder@gmail.com>
2019-12-30 16:07:59 -0600

    updated and corrected vars

--- sample/configure_login_banner.py
+++ sample/configure_login_banner.py
@@ -3,4 +3,4 @@ import rubrik_cdm
 rubrik = rubrik_cdm.Connect()
 
 bannerText = "Welcome To Rubrik"
-configure_ntp = rubrik.configure_login_banner(bannerText)
+configure_banner = rubrik.configure_login_banner(bannerText)
