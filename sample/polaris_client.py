8acbc75b7480c7ea6d47ea553ed2777cef0e9d7c
Ramzi Ferchichi <ramzi.ferchichi@rubrik.com>
2020-10-22 09:24:57 +0200

    Improved readability and added exception handling to the polaris.py functions. Also made sure functions only return one type instead of multiple depending on passed arguments (get_snapshots, get_sla_domains).

--- sample/polaris_client.py
+++ sample/polaris_client.py
@@ -1,11 +1,10 @@
 #! /usr/bin/env python3
-import sys
-import time
-from timeit import default_timer as timer
-
-import rubrik_polaris
 import argparse
 import pprint
+import sys
+
+import rubrik_polaris
+
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -33,9 +32,9 @@ except Exception as err:
 # rubrik.delete_account_aws(profiles = ['peterm-profile'])
 # rubrik.delete_account_aws(all = True )
 
-
-### Run ODS for machines in a region using Gold retention, monitor to complete via threads
-# pp.pprint(rubrik.submit_on_demand(rubrik.get_object_ids_azure(region="EastUS2"), rubrik.get_sla_domains("Bronze"), wait = True))
+### Run ODS for machines in a region using Bronze retention, monitor to complete via threads
+# bronze_sla_domain_id = rubrik.get_sla_domains("Bronze")[0]['id']
+# pp.pprint(rubrik.submit_on_demand(rubrik.get_object_ids_azure(region="EastUS2"), bronze_sla_domain_id, wait=True))
 
 ### Returns all objectIDs matching arbitrary available inputs. ec2 tags have special treatment
 # pp.pprint(rubrik.get_object_ids_ec2(tags = {"Name": "gurlingwinjb"}))
@@ -84,6 +83,5 @@ except Exception as err:
 # pp.pprint(rubrik.update_account_aws())
 
 ### Query objects, set sla_domain
-# pp.pprint(rubrik.submit_assign_sla( rubrik.get_object_ids_ec2(region = "US_WEST_2"), rubrik.get_sla_domains("Gold")))
-
-
+# gold_sla_domain_id = rubrik.get_sla_domains("Gold")[0]['id']
+# pp.pprint(rubrik.submit_assign_sla( rubrik.get_object_ids_ec2(region = "US_WEST_2"), gold_sla_domain_id))
--- sample/rubrik_polaris/get_snapshots.py
+++ sample/rubrik_polaris/get_snapshots.py
@@ -8,7 +8,8 @@ password = 's3cr3tP_a55w0R)'
 
 client = PolarisClient(domain, username, password, insecure=True)
 
-snappables = client.get_object_ids_ec2(tags = {"Environment": "staging"})
+snappables = client.get_object_ids_ec2(tags={"Environment": "staging"})
 for snappable in snappables:
-    snapshot_id = client.get_snapshots(snappable, recovery_point='latest')
-    print(snapshot_id)
+    snapshot = client.get_snapshots(snappable, recovery_point='latest')
+    if snapshot:
+        print(snapshot[0])
