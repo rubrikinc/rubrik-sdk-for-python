262f2baaaa87ddd4525af349ce656edb3567200c
Jaap Brasser <noreply@jaapbrasser.com>
2021-03-01 00:28:19 +0100

    Testing test

--- tests/unit/cloud_test.py
+++ tests/unit/cloud_test.py
@@ -16,7 +16,7 @@ def test_aws_s3_cloudout_invalid_aws_region(rubrik):
 
     error_message = error.value.args[0]
 
-    assert error_message == "The `aws_region` must be one of the following: ['us-gov-east-1','us-east-1','us-east-2','us-west-1','us-west-2','eu-west-1','eu-west-2','eu-west-3','eu-central-1','eu-north-1','eu-south-1','ap-east-1','ap-south-1','ap-southeast-1','ap-southeast-2','ap-northeast-1','ap-northeast-2','sa-east-1','cn-north-1','cn-northwest-1','ca-central-1','af-south-1','me-south-1']"
+    assert error_message == "The `aws_region` must be one of the following: ['us-gov-east-1', 'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-central-1', 'eu-north-1', 'eu-south-1', 'ap-east-1', 'ap-south-1', 'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ap-northeast-2', 'sa-east-1', 'cn-north-1', 'cn-northwest-1', 'ca-central-1', 'af-south-1', 'me-south-1']"
 
 
 def test_aws_s3_cloudout_invalid_storage_class(rubrik):
