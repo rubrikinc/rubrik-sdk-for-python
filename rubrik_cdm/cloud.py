62b60b27e9640e09a7675d1d660ff00c48ccc928
Jaap Brasser <noreply@jaapbrasser.com>
2021-02-28 23:54:16 +0100

    Added AWS region 🌏

--- rubrik_cdm/cloud.py
+++ rubrik_cdm/cloud.py
@@ -563,23 +563,29 @@ class Cloud(Api):
         self.function_name = inspect.currentframe().f_code.co_name
 
         valid_aws_regions = [
-            'ap-south-1',
-            'ap-northeast-3',
-            'ap-northeast-2',
-            'ap-southeast-1',
-            'ap-southeast-2',
-            'ap-northeast-1',
-            'ca-central-1',
-            'cn-north-1',
-            'cn-northwest-1',
-            'eu-central-1',
+            'us-gov-east-1',
+            'us-east-1',
+            'us-east-2',
+            'us-west-1',
+            'us-west-2',
             'eu-west-1',
             'eu-west-2',
             'eu-west-3',
-            'us-west-1',
-            'us-east-1',
-            'us-east-2',
-            'us-west-2']
+            'eu-central-1',
+            'eu-north-1',
+            'eu-south-1',
+            'ap-east-1',
+            'ap-south-1',
+            'ap-southeast-1',
+            'ap-southeast-2',
+            'ap-northeast-1',
+            'ap-northeast-2',
+            'sa-east-1',
+            'cn-north-1',
+            'cn-northwest-1',
+            'ca-central-1',
+            'af-south-1',
+            'me-south-1']
 
         # verify we are on cdm 4.2 or newer, required for cloud native
         # protection
